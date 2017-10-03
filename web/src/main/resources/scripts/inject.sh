6f779e8b9bbfe7e9c5ca7c3fd2669b0a1629e02d
Jess Sightler <jesse.sightler@gmail.com>
2017-10-03 18:39:00 -0400

    Moved the web openshift module into its own module and added dependency on the themes addon

--- web/src/main/resources/scripts/inject.sh
+++ web/src/main/resources/scripts/inject.sh
@@ -8,7 +8,7 @@ echo "Running keycloak server CLI script"
 ${JBOSS_HOME}/bin/jboss-cli.sh --file=${JBOSS_HOME}/bin/keycloak-install.cli
 
 echo "Installing keycloak client adapters"
-unzip -o -d /opt/eap/ /opt/tools/keycloak-client-overlay/keycloak-wildfly-adapter-dist-2.5.5.Final.zip
+unzip -o -d ${JBOSS_HOME} /opt/tools/keycloak-client-overlay/keycloak-wildfly-adapter-dist-2.5.5.Final.zip
 sed -i 's#embed-server --server-config=standalone.xml#embed-server --server-config=standalone-openshift.xml#g' ${JBOSS_HOME}/bin/adapter-install-offline.cli
 
 echo "Running keycloak client adapter cli script"
@@ -18,9 +18,20 @@ echo "Running local CLI script for configuring logging, queues, and keycloak cli
 # Run our CLI script (logging and keycloak configuration)
 ${JBOSS_HOME}/bin/jboss-cli.sh --file=${JBOSS_HOME}/standalone/configuration/setup.cli
 
+echo "Unzipping keycloak theme"
+unzip -o -d ${JBOSS_HOME} /opt/tools/keycloak-theme/keycloak-theme.jar
+cp ${JBOSS_HOME}/themes/rhamt/login/login_required.theme.properties ${JBOSS_HOME}/themes/rhamt/login/theme.properties
+
 echo "Setting up keycloak server admin username/password"
-java -jar /opt/tools/keycloak-tool/keycloak-tool.jar initialize-keycloak \
-    --username admin \
-    --password password \
-    --file \
-    ${JBOSS_HOME}/standalone/configuration/keycloak-add-user.json
+${JBOSS_HOME}/bin/add-user-keycloak.sh --realm master --user admin --password password
+
+echo "Setting up keycloak server rhamt default username/password"
+${JBOSS_HOME}/bin/add-user-keycloak.sh --realm rhamt --user rhamt --password password --roles user
+
+
+# If there is no DB prefix mapping, then setup an internal H2 Instance
+if [ -z ${DB_SERVICE_PREFIX_MAPPING+x} ]
+then
+    echo "Setting up embedded database, as the DB_SERVICE_PREFIX_MAPPING was not set"
+    ${JBOSS_HOME}/bin/jboss-cli.sh --file=${JBOSS_HOME}/standalone/configuration/db_h2.cli
+fi
