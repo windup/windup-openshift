70314354aac43775f0312c24f1eba8041cdfeb80
Jess Sightler <jesse.sightler@gmail.com>
2017-10-27 16:39:38 -0400

    Improved logging and fixed memory parameters

--- messaging-executor/src/main/resources/bin/livenessProbe.sh
+++ messaging-executor/src/main/resources/bin/livenessProbe.sh
@@ -5,14 +5,14 @@ PING_FILE=$PING_DIR/ping
 PONG_FILE=$PING_DIR/pong
 
 if [[ ! -d $PING_DIR ]] ; then
-    echo 'File "$PING_DIR" is not there, waiting a few seconds first.'
-    sleep 10
+#    echo "File \"$PING_DIR\" is not there, waiting a few seconds first."
+    sleep 20
 fi
 
 touch $PING_DIR/ping
 sleep 2
 
 if [[ ! -f $PONG_FILE ]] ; then
-    echo 'File "$PONG_FILE" is not there, aborting.'
+    echo "File \"$PONG_FILE\" is not there, aborting."
     exit 1
 fi
--- messaging-executor/src/main/resources/bin/openshift-launch.sh
+++ messaging-executor/src/main/resources/bin/openshift-launch.sh
@@ -8,14 +8,17 @@ JMS_HOST=${JMS_HOST^^}
 JMS_HOST=${JMS_HOST//-/_}
 JMS_HOST=${!JMS_HOST}
 
-DATA_DIR=/opt/rhamt-cli/data
+DATA_DIR=/opt/eap/standalone/data
 PING_DIR=/opt/rhamt-cli/ping
 
 echo "Starting executor with messaging server $JMS_HOST as user $JMS_USER"
 
 RHAMT_OPTS=`/opt/run-java/java-default-options`
+export RHAMT_OPTS="${RHAMT_OPTS/-XX:MaxMetaspaceSize=100m/-XX:MaxMetaspaceSize=256m}"
 
-/opt/rhamt-cli/bin/rhamt-cli -Dmessaging.serializer=amq.largemessage \
+echo "RHAMT_OPTS: $RHAMT_OPTS"
+
+/opt/rhamt-cli/bin/rhamt-cli -Dmessaging.serializer=shared.storage \
     -Dwindup.data.dir=${DATA_DIR} \
     --messagingExecutor \
     --user ${JMS_USER} --password ${JMS_PASSWORD} --host ${JMS_HOST} \
