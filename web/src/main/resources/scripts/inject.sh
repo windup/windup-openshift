cb52381c24b0e4758e9a8bbc684bf52d67e3cc71
jonathan <jvilalop@redhat.com>
2019-02-19 12:52:50 +0100

    Moved secure deployment EAP commands to another file to avoid an error "Cannot register capability"

--- web/src/main/resources/scripts/inject.sh
+++ web/src/main/resources/scripts/inject.sh
@@ -12,13 +12,15 @@ echo "Installing keycloak client adapters"
 unzip -o -d ${JBOSS_HOME} /opt/tools/keycloak-client-overlay/keycloak-wildfly-adapter-dist-*.zip
 sed -i 's#embed-server --server-config=${server.config:standalone.xml}#embed-server --server-config=standalone-openshift.xml#g' ${JBOSS_HOME}/bin/adapter-install-offline.cli
 
-
 echo "Running keycloak client adapter cli script"
 ${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/bin/adapter-install-offline.cli
 
 echo "Running local CLI script for configuring logging, queues, and keycloak client realm"
 # Run our CLI script (logging and keycloak configuration)
 ${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/standalone/configuration/setup.cli
+
+echo "Running secure-deployments : secure-deployments.cli "
+${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/standalone/configuration/secure-deployments.cli
 
 echo "Unzipping keycloak theme"
 unzip -o -d ${JBOSS_HOME} /opt/tools/keycloak-theme/keycloak-theme.jar
