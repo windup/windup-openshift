7a62e6889300b995f3fad9d841810a3fe9e4d49a
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2022-02-22 19:52:43 +0100

    restore standalone folder

--- web/src/main/resources/scripts/inject.sh
+++ web/src/main/resources/scripts/inject.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#mkdir -p ${JBOSS_HOME}/standalone/log
 
 echo "Installing keycloak server"
 unzip -o -d ${JBOSS_HOME} /opt/tools/keycloak-server-overlay/keycloak-server-overlay-*.zip
@@ -15,10 +14,10 @@ ${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/bin/adapter-i
 
 echo "Running local CLI script for configuring logging, queues, and keycloak client realm"
 # Run our CLI script (logging and keycloak configuration)
-${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/bin/configuration/setup.cli
+${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/standalone/configuration/setup.cli
 
 echo "Running secure-deployments : secure-deployments.cli "
-${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/bin/configuration/secure-deployments.cli
+${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/standalone/configuration/secure-deployments.cli
 
 echo "Unzipping keycloak theme"
 unzip -o -d ${JBOSS_HOME} /opt/tools/keycloak-theme/keycloak-theme.jar
@@ -39,11 +38,11 @@ ${JBOSS_HOME}/bin/add-user.sh -r ApplicationRealm -u jms-user -p gthudfal -g gue
 if [ -z ${DB_SERVICE_PREFIX_MAPPING+x} ]
 then
     echo "Setting up embedded database, as the DB_SERVICE_PREFIX_MAPPING was not set"
-    ${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/bin/configuration/db_h2.cli
+    ${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/standalone/configuration/db_h2.cli
 fi
 
 echo "Setting up as a master node"
-${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/bin/configuration/master.cli
+${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/standalone/configuration/master.cli
 
 echo "Configuring keycloak and openshift layers"
 sed -i -e 's#layers=keycloak#layers=keycloak,openshift#g' /opt/eap/modules/layers.conf
