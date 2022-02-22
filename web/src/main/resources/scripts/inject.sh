ae3e52aab5bf15f82e4791e40e6d18bf5e19cbc0
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2022-02-22 17:57:31 +0100

    secure deployments working

--- web/src/main/resources/scripts/inject.sh
+++ web/src/main/resources/scripts/inject.sh
@@ -10,13 +10,12 @@ echo "Running keycloak server CLI script"
 ${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/bin/keycloak-install.cli
 
 #echo "Installing keycloak client adapters"
-#unzip -o -d ${JBOSS_HOME} /opt/tools/keycloak-client-overlay/keycloak-wildfly-adapter-dist-*.zip
-#sed -i 's#embed-server --server-config=${server.config:standalone.xml}#embed-server --server-config=${server.config:standalone-openshift.xml}#g' ${JBOSS_HOME}/bin/adapter-install-offline.cli
-#${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/bin/adapter-install-offline.cli
+unzip -o -d ${JBOSS_HOME} /opt/tools/keycloak-client-overlay/keycloak-wildfly-adapter-dist-*.zip
+sed -i 's#embed-server --server-config=${server.config:standalone.xml}#embed-server --server-config=${server.config:standalone-openshift.xml}#g' ${JBOSS_HOME}/bin/adapter-install-offline.cli
+${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/bin/adapter-install-offline.cli
 
 echo "Running local CLI script for configuring logging, queues, and keycloak client realm"
 # Run our CLI script (logging and keycloak configuration)
-#sed -i 's#<subsystem xmlns="urn:jboss:domain:messaging-activemq:13.0"/>#<subsystem xmlns="urn:jboss:domain:messaging-activemq:13.0"><server name="default"><journal pool-files="10"/><statistics enabled="${wildfly.messaging-activemq.statistics-enabled:${wildfly.statistics-enabled:false}}"/><security-setting name="\#"><role name="guest" send="true" consume="true" create-non-durable-queue="true" delete-non-durable-queue="true"/></security-setting><address-setting name="\#" dead-letter-address="jms.queue.DLQ" expiry-address="jms.queue.ExpiryQueue" max-size-bytes="10485760" page-size-bytes="2097152" message-counter-history-day-limit="10" redistribution-delay="1000"/><http-connector name="http-connector" socket-binding="http-messaging" endpoint="http-acceptor"/><http-connector name="http-connector-throughput" socket-binding="http-messaging" endpoint="http-acceptor-throughput"><param name="batch-delay" value="50"/></http-connector><in-vm-connector name="in-vm" server-id="0"><param name="buffer-pooling" value="false"/></in-vm-connector><http-acceptor name="http-acceptor" http-listener="default"/><http-acceptor name="http-acceptor-throughput" http-listener="default"><param name="batch-delay" value="50"/><param name="direct-deliver" value="false"/></http-acceptor><in-vm-acceptor name="in-vm" server-id="0"><param name="buffer-pooling" value="false"/></in-vm-acceptor><jms-queue name="ExpiryQueue" entries="java:/jms/queue/ExpiryQueue"/><jms-queue name="DLQ" entries="java:/jms/queue/DLQ"/><connection-factory name="InVmConnectionFactory" entries="java:/ConnectionFactory" connectors="in-vm"/><connection-factory name="RemoteConnectionFactory" entries="java:jboss/exported/jms/RemoteConnectionFactory" connectors="http-connector" reconnect-attempts="-1"/><pooled-connection-factory name="activemq-ra" entries="java:/JmsXA java:jboss/DefaultJMSConnectionFactory" connectors="in-vm" transaction="xa"/></server></subsystem>#g' /opt/eap/standalone/configuration/standalone-openshift.xml
 ${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/bin/configuration/setup.cli
 
 #echo "Running secure-deployments : secure-deployments.cli "
