014a377f9e8ccc797937e37685e1422fb286aa83
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2022-07-14 10:48:41 +0200

    [WINDUP-3363]-MTA - support multiple themes (#47)
    * Allow multiple themes
    
    * No need to use mvn property in CLI
    
    * Use correct cli artifact
    
    * Update versions

--- web/src/main/resources/scripts/inject.sh
+++ web/src/main/resources/scripts/inject.sh
@@ -21,13 +21,18 @@ ${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/standalone/co
 
 echo "Unzipping keycloak theme"
 unzip -o -d ${JBOSS_HOME} /opt/tools/keycloak-theme/keycloak-theme.jar
+for theme in $(ls ${JBOSS_HOME}/themes)
+do
+  cp ${JBOSS_HOME}/themes/$theme/login/login_required.theme.properties ${JBOSS_HOME}/themes/$theme/login/theme.properties
+done
+
 cp ${JBOSS_HOME}/themes/mta/login/login_required.theme.properties ${JBOSS_HOME}/themes/mta/login/theme.properties
 
 echo "Setting up keycloak server admin username/password"
 ${JBOSS_HOME}/bin/add-user-keycloak.sh --realm master --user admin --password password
 
 echo "Setting up keycloak server mta default username/password"
-${JBOSS_HOME}/bin/add-user-keycloak.sh --realm mta --user mta --password password --roles user
+${JBOSS_HOME}/bin/add-user-keycloak.sh --realm mta --user migration --password password --roles user
 
 echo "Setting up JMS Password"
 ${JBOSS_HOME}/bin/add-user.sh -r ApplicationRealm -u jms-user -p gthudfal -g guest \
