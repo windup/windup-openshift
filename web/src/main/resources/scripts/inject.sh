63ff5e3176d871f119a835c1509fe75cd64733a2
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2022-08-04 10:09:58 +0200

    Use JKube instead of Fabric8 to apply permissions and have a reliable build

--- web/src/main/resources/scripts/inject.sh
+++ web/src/main/resources/scripts/inject.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 echo "Installing keycloak server"
-unzip -o -d ${JBOSS_HOME} /opt/tools/keycloak-server-overlay/keycloak-server-overlay-*.zip
+unzip -o -d ${JBOSS_HOME} ${JBOSS_HOME}/tools/keycloak-server-overlay/keycloak-server-overlay-*.zip
 sed -i 's#embed-server --server-config=standalone.xml#embed-server --server-config=standalone-openshift.xml#g' ${JBOSS_HOME}/bin/keycloak-install.cli
 
 echo "Running keycloak server CLI script"
 ${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/bin/keycloak-install.cli
 
 echo "Installing keycloak client adapters"
-unzip -o -d ${JBOSS_HOME} /opt/tools/keycloak-client-overlay/keycloak-wildfly-adapter-dist-*.zip
+unzip -o -d ${JBOSS_HOME} ${JBOSS_HOME}/tools/keycloak-client-overlay/keycloak-wildfly-adapter-dist-*.zip
 sed -i 's#embed-server --server-config=${server.config:standalone.xml}#embed-server --server-config=${server.config:standalone-openshift.xml}#g' ${JBOSS_HOME}/bin/adapter-install-offline.cli
 ${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/bin/adapter-install-offline.cli
 
 echo "Running local CLI script for configuring logging, queues, and keycloak client realm"
 # Run our CLI script (logging and keycloak configuration)
-${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/standalone/configuration/setup.cli
+${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/utils/configuration/setup.cli
 
 echo "Running secure-deployments : secure-deployments.cli "
-${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/standalone/configuration/secure-deployments.cli
+${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/utils/configuration/secure-deployments.cli
 
 echo "Unzipping keycloak theme"
-unzip -o -d ${JBOSS_HOME} /opt/tools/keycloak-theme/keycloak-theme.jar
+unzip -o -d ${JBOSS_HOME} ${JBOSS_HOME}/tools/keycloak-theme/keycloak-theme.jar
 for theme in $(ls ${JBOSS_HOME}/themes)
 do
   cp ${JBOSS_HOME}/themes/$theme/login/login_required.theme.properties ${JBOSS_HOME}/themes/$theme/login/theme.properties
@@ -41,11 +41,11 @@ ${JBOSS_HOME}/bin/add-user.sh -r ApplicationRealm -u jms-user -p gthudfal -g gue
 if [ -z ${DB_SERVICE_PREFIX_MAPPING+x} ]
 then
     echo "Setting up embedded database, as the DB_SERVICE_PREFIX_MAPPING was not set"
-    ${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/standalone/configuration/db_h2.cli
+    ${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/utils/configuration/db_h2.cli
 fi
 
 echo "Setting up as a master node"
-${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/standalone/configuration/master.cli
+${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/utils/configuration/master.cli
 
 if [ -d "${JBOSS_HOME}/modules/system/layers/openshift/" ]; then
   echo "Configuring keycloak and openshift layers"
--- web/src/main/resources/scripts/webapp-launch.sh
+++ web/src/main/resources/scripts/webapp-launch.sh
@@ -6,7 +6,7 @@ cd $DIR
 export JAVA_OPTS="$JAVA_OPTS -XX:MaxMetaspaceSize=512m -XX:ReservedCodeCacheSize=512m"
 export JAVA_OPTS="$JAVA_OPTS -Djboss.modules.system.pkgs=org.jboss.byteman"
 export JAVA_OPTS="$JAVA_OPTS -Djboss.modules.settings.xml.url=file://$GALLEON_MAVEN_SETTINGS_XML"
-export JAVA_OPTS="$JAVA_OPTS -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=${JBOSS_HOME}/standalone/configuration/import-realm.json -Dkeycloak.migration.strategy=IGNORE_EXISTING"
+export JAVA_OPTS="$JAVA_OPTS -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=${JBOSS_HOME}/kc-configuration/import-realm.json -Dkeycloak.migration.strategy=IGNORE_EXISTING"
 
 export MAVEN_SETTINGS_PATH="$GALLEON_MAVEN_SETTINGS_XML"
 
@@ -14,10 +14,13 @@ echo 'echo "Installing PostgreSQL driver"' >> ${JBOSS_HOME}/bin/launch/launch.sh
 echo "${JBOSS_HOME}/bin/jboss-cli.sh --echo-command --file=${JBOSS_HOME}/driver/db_postgresql.cli" >> ${JBOSS_HOME}/bin/launch/launch.sh
 
 echo 'echo "Installing Required Configuration"' >> ${JBOSS_HOME}/bin/launch/launch.sh
-echo "source ${JBOSS_HOME}/bin/inject.sh" >> ${JBOSS_HOME}/bin/launch/launch.sh
+echo "source ${JBOSS_HOME}/scripts/inject.sh" >> ${JBOSS_HOME}/bin/launch/launch.sh
 
 if [ ! -f "${JBOSS_HOME}/standalone/configuration/standalone-openshift.xml" ]; then
   ln -s ${JBOSS_HOME}/standalone/configuration/standalone.xml ${JBOSS_HOME}/standalone/configuration/standalone-openshift.xml
 fi
+
+cp "${JBOSS_HOME}/wars/api.war" /deployments/api.war
+cp "${JBOSS_HOME}/wars/windup-ui.war" /deployments/windup-ui.war
 
 ${JBOSS_HOME}/bin/openshift-launch.sh "$@"
