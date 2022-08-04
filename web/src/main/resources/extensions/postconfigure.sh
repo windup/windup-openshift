2d603c6f8c2d44fdacafd7de4d9c759a9d4ae434
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2022-08-04 15:30:24 +0200

    Make Container Images Wildfly Based (#49)
    * Replace EAP references by $JBOSS_HOME
    
    * Fix script
    
    * Add custom standalone.xml
    
    * Added 'messaging-activemq' subsystem configuration
    
    * Added 'messaging-activemq' subsystem configuration (#1)
    
    * Remove standalone.xml, add probes, and fix jms config
    
    * Change probe
    
    * Add EAP compatibility changes
    
    * Add profile to build EAP based container images
    
    * Use JBOSS_HOME and change DATA_DIR
    
    * Use JBOSS_HOME
    
    * Generate container image from PRs
    
    * Build project dependencies before image
    
    * Change permissions
    
    * Change permissions
    
    * Change permissions
    
    * Change permissions
    
    * Return to user jboss at the end
    
    * Change permissions
    
    * Change permissions
    
    * Remove GH actions
    
    * Changing deployments permissions
    
    * Use proper permissions when building the containers
    
    * Changed 'Windup' label in OCP templates (#2)
    
    * Use JKube instead of Fabric8 to apply permissions and have a reliable build
    
    * Refactored 'version.jkube' to be defined only once and set 'dockerHost' in 'podman' profile (#3)
    
    Co-authored-by: mrizzi <mrizzi@redhat.com>
    Co-authored-by: Marco Rizzi <mrizzi@users.noreply.github.com>

--- web/src/main/resources/extensions/postconfigure.sh
+++ web/src/main/resources/extensions/postconfigure.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -x
 echo "Creating Windup Queues and Topics"
-$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_HOME/extensions/jms.cli
+$JBOSS_HOME/bin/jboss-cli.sh --echo-command --file=$JBOSS_HOME/extensions/jms.cli
